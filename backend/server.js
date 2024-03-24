46ddb5db103e38c7694e73cd04812a9becbac9bb
NegrebLC <BergCu2001@gmail.com>
2024-03-23 22:23:19 -0600

    Setup

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+const app = express();
+
+// Connect to database
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Define routes
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,8 @@
+//this function returns the footer for the layout
+export default function Footer() {
+  return (
+    <footer className="footer text-muted">
+      <div className="container">&copy; 2024 - Creatures</div>
+    </footer>
+  );
+}
