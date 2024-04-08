3aef41c64dce5949c50c4dc6dcc0fa4d744feb97
Syyren <129496054+Syyren@users.noreply.github.com>
2024-04-08 13:39:49 -0600

    updated home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,7 +3,11 @@ import Layout from "../components/Layout";
 export default function Home() {
   return (
     <Layout>
-      <h2 className="display-4 mb-4">Home Page</h2>
+      <h2 className="display-3 mb-4 text-center">Hello!</h2>
+      <h3 className="display-4 mb-4 text-center"> and Welcome to the Wild World of...</h3>
+      <h1 className="display-1 mb-4 text-center">Fugglets!</h1>
+      <p className="text-center">These fun little guys are just waiting to meet you.</p>
+      <p className="text-center">So go ahead and click on 'My Fugglet' to get started!</p>
     </Layout>
   );
 }
