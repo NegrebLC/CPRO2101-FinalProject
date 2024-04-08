23dc8e50523104e8493f552446fec0a99ccb6c13
Syyren <129496054+Syyren@users.noreply.github.com>
2024-04-08 13:53:28 -0600

    updated responsive design

--- frontend/src/pages/Logout.js
+++ frontend/src/pages/Logout.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Layout from "../components/Layout";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
@@ -18,15 +19,21 @@ export default function Logout() {
   };
 
   return (
-    <div className="text-center">
-      <h2>Logout</h2>
-      <p>Are you sure you want to logout?</p>
-      <button className="btn btn-secondary mx-2" onClick={handleLogout}>
-        Logout
-      </button>
-      <button className="btn btn-secondary mx-2" onClick={handleCancel}>
-        Cancel
-      </button>
-    </div>
+    <Layout>
+      <div className="container-fluid h-100 text-center align-items-center justify-content-center">
+        <h2>Logout</h2>
+        <p>Are you sure you want to logout?</p>
+        <div className="container col-md-2 mb-2">
+          <button className="btn btn-secondary mx-2" onClick={handleLogout}>
+            Logout
+          </button>
+        </div>
+        <div className="container col-md-2">
+          <button className="btn btn-secondary mx-2" onClick={handleCancel}>
+            Cancel
+          </button>
+        </div>
+      </div>
+    </Layout>
   );
 }
